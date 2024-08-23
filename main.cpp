83f460712a51ddf9b504d366631c467273f1046b
Rexcodi <adrian.rodriguez22@upr.edu>
2024-08-23 16:17:34 +0000

    commit

--- main.cpp
+++ main.cpp
@@ -1,7 +1,9 @@
-//Adrian Rodriguez
+//Adrian Rodriguez (adrian.rodriguez22@upr.edu)
 //CCOM 3033 - Lab 01
+//A program to practice modifications and compilation
+
 #include <iostream>
 
 int main() {
-  std::cout << "Hello World!\n";
+  std::cout << "Hello Adrian!\n";
 }
